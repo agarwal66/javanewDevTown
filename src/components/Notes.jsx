903f532b2ade0ebc27c2eeb5c604a576ec426655
Prateek Agarwal <114166286+agarwal66@users.noreply.github.com>
2022-12-05 22:09:05 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+function Notes() {
+  return (
+    <div className="note">
+      <h1>DeepLearning</h1>
+      <p>
+        Deep learning is a subset of machine learning, which is essentially a
+        neural network with three or more layers. These neural networks attempt
+        to simulate the behavior of the human brain—albeit far from matching its
+        ability—allowing it to “learn” from large amounts of data. While a
+        neural network with a single layer can still make approximate
+        predictions, additional hidden layers can help to optimize and refine
+        for accuracy.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react-dom";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
